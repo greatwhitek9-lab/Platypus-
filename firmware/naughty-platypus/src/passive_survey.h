a1867502ece9e78f3692a58875d95910cfe352c8
UrbanPoacher <greatwhitek9@gmail.com>
2026-07-10 02:32:10 +0000

    Add active scan mode firmware API

--- firmware/naughty-platypus/src/passive_survey.h
+++ firmware/naughty-platypus/src/passive_survey.h
@@ -7,6 +7,10 @@ int np_passive_survey_start(void);
 int np_passive_survey_stop(void);
 int np_passive_survey_status(void);
 int np_passive_survey_reset(void);
+int np_passive_survey_drain(void);
+int np_passive_survey_set_active(void);
+int np_passive_survey_set_passive(void);
+int np_passive_survey_mode_status(void);
 bool np_passive_survey_is_running(void);
 
 #endif
